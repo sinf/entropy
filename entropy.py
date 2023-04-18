42a1f1de53d131d4877c3339fd5fe228e05a9855
ArhoM <root@localhost>
2023-04-18 21:05:09 +0300

    hilbert curve heatmap , limit progress updates to 4 Hz

--- entropy.py
+++ entropy.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#vim:set et sw=4 ts=4:
 import time
 import os
 from argparse import ArgumentParser, ArgumentTypeError
@@ -55,26 +56,51 @@ def read_mmap(filename, block_size):
 def get_entropy(data):
     return entropy2(np.frombuffer(data, dtype='uint8'), base=2)
 
-class tqdm: # barebones tqdm clone
-    @staticmethod
-    def tqdm(iterable, total):
-        i=0
-        for it in iterable:
-            i += 1
+def tqdm(iterable, total): # barebones tqdm clone
+    nextpt=time.time()
+    i=0
+    for it in iterable:
+        i += 1
+        now=time.time()
+        if now > nextpt:
+            nextpt += 0.25
             print(f'\033[1K\r{i}/{total} {i/total*100:02.2f}%', end='')
-            yield it
-        print()
+        yield it
+    print(f'\033[1K\r{i}/{total} {i/total*100:02.2f}%')
 
 def filesize(path): # works with both regular and block devices
     with open(path, 'rb') as f:
         f.seek(0, 2)
         return f.tell()
 
+def d2xy(n, d):
+    """ get (x,y) coordinate on hilbert curve. copy paste translate from wikipedia """
+    t=d
+    x,y=0,0
+    s=1
+    while s < n:
+        rx = 1 & (t >> 1)
+        ry = 1 & (t ^ rx)
+        if ry == 0:
+            if rx == 1:
+                x = s-1 - x
+                y = s-1 - y
+            x,y = y,x
+        x += s * rx
+        y += s * ry
+        t >>= 2
+        s *= 2
+    return x,y
+
+def next_power_of_2(x):  
+    return 1 if x == 0 else 2**(x - 1).bit_length()
+
 def main():
     ap=ArgumentParser()
     ap.add_argument('-b', '--block-size', type=parse_size, default='1M')
     ap.add_argument('-p', '--processes', type=int, default=os.cpu_count())
     ap.add_argument('-g', '--graph', default=False, action='store_true')
+    ap.add_argument('-G', '--hilbert-curve-heatmap', default=False, action='store_true')
     ap.add_argument('-c', '--output-csv', type=str, metavar='FILENAME')
     ap.add_argument('filename')
     args=ap.parse_args()
@@ -82,7 +108,11 @@ def main():
 
     t0 = time.time()
     size = filesize(args.filename)
-    n = (size + args.block_size - 1) // args.block_size
+    n = size // args.block_size
+    
+    if n < 1:
+        print("block size > file size. will not")
+        exit(42)
 
     blocks = read_mmap(args.filename, args.block_size)
     #blocks = read_file(args.filename, args.block_size)
@@ -90,7 +120,7 @@ def main():
     print('processes:', args.processes)
     with Pool(args.processes) as p:
         entropys = []
-        for x in tqdm.tqdm(p.imap(get_entropy, blocks, chunksize=128), total=n):
+        for x in tqdm(p.imap(get_entropy, blocks, chunksize=128), total=n):
             entropys += [x]
 
     t1 = time.time()
@@ -112,6 +142,19 @@ def main():
         plt.plot(entropys)
         plt.ylabel('entropyBase2')
         plt.xlabel('blocknr')
+        plt.show()
+
+    if args.hilbert_curve_heatmap:
+        s=next_power_of_2(int(np.sqrt(n)))
+        raster=np.zeros((s,s))
+        entropys_interp = np.interp(np.arange(0,s*s)/(s*s-1), np.arange(0,n)/(n-1), entropys)
+        #lo,hi = min(entropys) , max(entropys)
+        lo,hi = 0,8
+        for i in range(raster.size):
+            x,y=d2xy(s, i)
+            raster[y,x] = (entropys_interp[i] - lo) / (hi - lo)
+        import matplotlib.pyplot as plt
+        plt.imshow(raster)
         plt.show()
 
 if __name__=="__main__":
